ebf8adf430293f36c7cbbca0e3e93b5a85534642
Ram4GB <leminhcuong2988@yahoo.com>
2022-04-22 14:44:27 +0700

    use public domain BE

--- src/service/fetch.js
+++ src/service/fetch.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   // for locally
-  baseURL: "http://localhost:3001/",
+  // baseURL: "http://localhost:3001/",
 
   // public
-  // baseURL: 'https://paint-ionized-rainstorm.glitch.me',
+  baseURL: "https://paint-ionized-rainstorm.glitch.me",
   timeout: 30000,
 });
 
